ce5dd6dd92deec0cd7b8b179deb3986c6d94a970
Alessandro Baffa <alessandro.baffa@gmail.com>
2021-06-07 12:22:31 +0900

    Fixed the vaultCustomQueryCriteria in Utilities.QueryConsumed(). All works.

--- workflows/src/main/kotlin/net/corda/samples/carinsurance/flows/Utilities.kt
+++ workflows/src/main/kotlin/net/corda/samples/carinsurance/flows/Utilities.kt
@@ -1,12 +1,11 @@
 package net.corda.samples.carinsurance.flows
 
-import net.corda.core.contracts.StateAndRef
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.InitiatingFlow
 import net.corda.core.node.services.Vault
 import net.corda.core.node.services.queryBy
-import net.corda.core.node.services.vault.*
 import net.corda.core.node.services.vault.Builder.equal
+import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.samples.carinsurance.schema.InsuranceSchemaV1
 import net.corda.samples.carinsurance.states.InsuranceState
 
@@ -14,34 +13,14 @@ import net.corda.samples.carinsurance.states.InsuranceState
 @InitiatingFlow
 class QueryConsumed(private val policyNumber: String, private val status: Vault.StateStatus) : FlowLogic<Int>() {
     override fun call(): Int {
-            val generalQuery = QueryCriteria.VaultQueryCriteria(status)
-            val index = InsuranceSchemaV1.PersistentInsurance::policyNumber.equal(policyNumber)
-            val customCriteria = QueryCriteria.VaultCustomQueryCriteria(index)
-            val criteria = generalQuery.and(customCriteria)
-            val pageSpec = PageSpecification(DEFAULT_PAGE_NUM, MAX_PAGE_SIZE)
-            val states = serviceHub.vaultService.queryBy<InsuranceState>(criteria, pageSpec).states
-            return states.size
-    }
-}
+        val generalQuery = QueryCriteria.VaultQueryCriteria(Vault.StateStatus.ALL)
+        val index = InsuranceSchemaV1.PersistentInsurance::policyNumber.equal(policyNumber)
 
-@InitiatingFlow
-class QueryAll() : FlowLogic<List<StateAndRef<InsuranceState>>>() {
-    override fun call(): List<StateAndRef<InsuranceState>> {
-        val generalQuery = QueryCriteria.VaultQueryCriteria(Vault.StateStatus.CONSUMED)
-        val states = serviceHub.vaultService.queryBy<InsuranceState>(generalQuery).states
-        return states
-    }
-}
+        /* IMPORTANT: the StateStatus needs to be passed to the VaultCustomQueryCriteria because it sets it to UNCONSUMED by default */
+        val customCriteria = QueryCriteria.VaultCustomQueryCriteria(index, status)
 
-@InitiatingFlow
-class QueryUnconsumed2(val policyNumber: String) : FlowLogic<Int>() {
-    override fun call(): Int {
-        return builder {
-            val generalQuery = QueryCriteria.VaultQueryCriteria(Vault.StateStatus.UNCONSUMED)
-            val index = InsuranceSchemaV1.PersistentInsurance::policyNumber.equal(policyNumber)
-            val customCriteria = QueryCriteria.VaultCustomQueryCriteria(index)
-            val criteria = generalQuery.and(customCriteria)
-            serviceHub.vaultService.queryBy<InsuranceState>(criteria)
-        }.states.size
+        val criteria = generalQuery.and(customCriteria)
+        val states = serviceHub.vaultService.queryBy<InsuranceState>(criteria).states
+        return states.size
     }
 }
